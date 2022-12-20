f1020e4fb3c07efe43d863333012242ab7f06c8b
Damien <90845231+Damien-Dupont@users.noreply.github.com>
2022-12-21 00:09:21 +0100

    changes to roles

--- src/backend/nounou.sql
+++ src/backend/nounou.sql
@@ -124,10 +124,10 @@ DROP TABLE IF EXISTS `role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `role` (
-  `idRole` int NOT NULL AUTO_INCREMENT,
+  `id_role` int NOT NULL AUTO_INCREMENT,
   `role` varchar(45) DEFAULT NULL COMMENT 'Admin\nParent\nassMat\n',
-  PRIMARY KEY (`idRole`),
-  UNIQUE KEY `id_role_UNIQUE` (`idRole`),
+  PRIMARY KEY (`id_role`),
+  UNIQUE KEY `id_role_UNIQUE` (`id_role`),
   UNIQUE KEY `role_UNIQUE` (`role`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
